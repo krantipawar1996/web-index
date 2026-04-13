a43dcdb5c9c9026ed08c5e4be929d92262af913f
krantipawar1996 <krantipawar1996@gmail.com>
2026-04-13 13:11:23 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,14 @@
+let nm = document.querySelector("#name");
+let form = document.querySelector("form");
+
+
+form.addEventListener("submit", function (dets) {
+  dets.preventDefault();
+
+  if (nm.value.length <= 2) {
+  
+
+
+  }
+  
+});
